e3b49ac642759418d4693fd6756a1ce0c9413172
Марина <marisha.sosnina@gmail.com>
2022-09-18 00:53:37 +0700

    simple navigation

---
+++ 04 Navigation/simple_navigation/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Routing and Navigation'),
+        ),
+        body: HomePage(),
+      ),
+      initialRoute: '/page2',
+      routes: {
+        '/page2': (context) => Page2(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(onPressed: (){
+        Navigator.pushNamed(context, '/page2');
+      },
+      child: Text('Move to Page 2')),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 2'),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          child: Text('Go Back'),
+        ),
+      ),
+    );
+  }
+}
