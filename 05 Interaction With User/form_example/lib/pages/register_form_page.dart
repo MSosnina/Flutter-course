ed6d7d1b411e0040b95eb65676aabb7a28262950
Марина <marisha.sosnina@gmail.com>
2022-09-27 08:12:17 +0700

    input fields styling

--- 05 Interaction With User/form_example/lib/pages/register_form_page.dart
+++ 05 Interaction With User/form_example/lib/pages/register_form_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/material/material_state.dart' ;
+import 'package:flutter/src/material/material_state.dart';
 
 class RegisterFormPage extends StatefulWidget {
   @override
@@ -19,33 +19,90 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
           padding: EdgeInsets.all(16),
           children: [
             TextField(
-              decoration: InputDecoration(labelText: 'Full Name *'),
+              decoration: InputDecoration(
+                labelText: 'Full Name *',
+                hintText: 'What do people call you?',
+                prefixIcon: Icon(Icons.person),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(color: Colors.black, width: 2)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(color: Colors.blue, width: 2)),
+              ),
             ),
             SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Phone Number *'),
+              decoration: InputDecoration(
+                labelText: 'Phone Number *',
+                hintText: 'Where we can reach you?',
+                helperText: 'Phone format: (XXX)XXX-XXXX',
+                prefixIcon: Icon(Icons.call),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(color: Colors.black, width: 2)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(color: Colors.blue, width: 2)),
+              ),
             ),
             SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Email Address'),
-            ),
+                decoration: InputDecoration(
+              labelText: 'Email Address',
+              hintText: 'Enter an email address',
+              icon: Icon(Icons.mail),
+            )),
             SizedBox(height: 20),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Life Story'),
+              decoration: InputDecoration(
+                labelText: 'Life Story',
+                hintText: 'Tell us about yourself',
+                helperText: 'Keep it short, this is just a demo',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 3,
             ),
             SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Password *'),
+              decoration: InputDecoration(
+                  labelText: 'Password *',
+                  hintText: 'Enter the password',
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.visibility),
+                    onPressed: () {},
+                  ),
+                  icon: Icon(Icons.security)),
             ),
             SizedBox(height: 10),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Confirm Password *'),
+              decoration: InputDecoration(
+                  labelText: 'Confirm Password *',
+                  hintText: 'Confirm the password',
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.visibility),
+                    onPressed: () {},
+                  ),
+                  icon: Icon(Icons.border_color)),
             ),
             SizedBox(height: 15),
             TextButton(
               onPressed: () {},
-              child: Text('Submit Form', style: TextStyle(color: Colors.white),),
-              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green)),
+              child: Text(
+                'Submit Form',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green)),
             ),
           ],
         ),
