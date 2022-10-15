b1b5791e9c24bcf4fd8da75ee8c452ba6398bdb9
Марина <marisha.sosnina@gmail.com>
2022-10-15 22:33:34 +0700

    homework

--- 05 Interaction With User/form_example/lib/pages/user_info_page.dart
+++ 05 Interaction With User/form_example/lib/pages/user_info_page.dart
@@ -1,15 +1,17 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../model/user.dart';
 
-class UserInfoPage extends StatelessWidget{
-
+class UserInfoPage extends StatelessWidget {
   User userInfo;
+
   UserInfoPage({required this.userInfo});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('User Info'),
@@ -20,31 +22,42 @@ class UserInfoPage extends StatelessWidget{
         child: Column(
           children: [
             ListTile(
-              title: Text('${userInfo.name}', style: TextStyle(fontWeight: FontWeight.w500)),
+              title: Text('${userInfo.name}',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
               subtitle: Text('${userInfo.story}'),
               leading: Icon(
                 Icons.person,
                 color: Colors.black,
               ),
-              trailing: Text('${userInfo.country}'),
+              trailing: Text('${userInfo.country == null ? '' : userInfo.country}'),
             ),
             ListTile(
-              title: Text('${userInfo.phone}', style: TextStyle(fontWeight: FontWeight.w500)),
+              title: Text('${userInfo.phone}',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
               leading: Icon(
                 Icons.phone,
                 color: Colors.black,
               ),
             ),
-            ListTile(
-              title: Text('${userInfo.email.isEmpty ? 'Not specified' : userInfo.email}', style: TextStyle(fontWeight: FontWeight.w500)),
-              leading: Icon(
-                Icons.mail,
-                color: Colors.black,
-              ),
-            )
+            _mail(userInfo.email),
           ],
         ),
       ),
     );
+  }
+
+  // ignore: curly_braces_in_flow_control_structures
+  Widget _mail(String mail) {
+    if (mail.isEmpty)
+      return SizedBox(height: 0);
+    else
+      return ListTile(
+        title: Text('${mail.isEmpty ? '' : mail}',
+            style: TextStyle(fontWeight: FontWeight.w500)),
+        leading: Icon(
+          Icons.mail,
+          color: Colors.black,
+        ),
+      );
   }
 }
