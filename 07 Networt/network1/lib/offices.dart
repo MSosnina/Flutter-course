2684f191ed50d24ad3b1bb14dfccec4a5c369a35
Марина <marisha.sosnina@gmail.com>
2022-11-13 18:13:38 +0700

    07_05

--- 07 Networt/network1/lib/offices.dart
+++ 07 Networt/network1/lib/offices.dart
@@ -1,13 +1,22 @@
-import 'dart:convert';
+library offices;
 
+import 'dart:convert';
+import 'package:json_annotation/json_annotation.dart';
 import 'package:http/http.dart' as http;
 
+part 'offices.g.dart';
+
+@JsonSerializable()
 class OfficesList {
   List<Office> offices;
 
   OfficesList({required this.offices});
 
-  factory OfficesList.fromJson(Map<String, dynamic> json){
+  factory OfficesList.fromJson(Map<String, dynamic> json) => _$OfficesListFromJson(json);
+
+  Map<String,dynamic> toJson() => _$OfficesListToJson(this);
+  /*
+  {
     var officesJson = json['offices'] as List;
 
     List<Office> officesList = officesJson.map((i) => Office.fromJson(i))
@@ -17,15 +26,20 @@ class OfficesList {
         offices: officesList
     );
   }
+  */
 }
 
+@JsonSerializable()
 class Office {
+//  @JsonKey(name: 'dt') //dt
+//  DateTime dateTime;
   final String? name;
   final String? address;
   final String? image;
 
   Office({this.name, this.address, this.image});
 
+  /*
   factory Office.fromJson(Map<String, dynamic> json){
     return Office(
         name: json['name'] as String,
@@ -33,11 +47,15 @@ class Office {
         image: json['image'] as String
     );
   }
+  */
+  factory Office.fromJson(Map<String, dynamic> json) => _$OfficeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$OfficeToJson(this);
 }
 
 Future<OfficesList> getOfficesList() async {
   Uri url = Uri.parse('https://about.google/static/data/locations.json');
-  final response = await http.get(url!);
+  final response = await http.get(url);
 
   if (response.statusCode == 200) {
     return OfficesList.fromJson(json.decode(response.body));
